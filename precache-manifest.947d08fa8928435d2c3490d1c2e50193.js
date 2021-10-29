fe88a7062ae03798ff2ccf8301c37334357fa29a
jdefi <83540733+jdefi@users.noreply.github.com>
2021-10-29 22:39:25 +0000

    Deploying to gh-pages from @ Allswap/beta-pages@cd22802b6f3594d079ef1dc202811ce7476892e7 🚀

---
+++ precache-manifest.947d08fa8928435d2c3490d1c2e50193.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9273e57527e89f466e5ffd45cc08e1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "318159f60f161b29a7f9",
+    "url": "/static/css/main.9bc6d313.chunk.css"
+  },
+  {
+    "revision": "e7e8689bb7f38992903e",
+    "url": "/static/js/0.a1fee327.chunk.js"
+  },
+  {
+    "revision": "6dacc6d62d184dcc0b13",
+    "url": "/static/js/10.787392d9.chunk.js"
+  },
+  {
+    "revision": "69d795edc38933cf549e",
+    "url": "/static/js/11.f8169a2a.chunk.js"
+  },
+  {
+    "revision": "840fee68b2b747c2bb0b",
+    "url": "/static/js/3.3f65f1de.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "/static/js/3.3f65f1de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d513f20421bd4e0434f",
+    "url": "/static/js/4.82851e7c.chunk.js"
+  },
+  {
+    "revision": "8e642a5b027fc2c4ceb3",
+    "url": "/static/js/5.e1becbae.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/static/js/5.e1becbae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763278e62fc1f102f79c",
+    "url": "/static/js/6.feaac263.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.feaac263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29e2fb298dc6f75f74a",
+    "url": "/static/js/7.4af9195a.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.4af9195a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f83918e14affc00969",
+    "url": "/static/js/8.67681b59.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.67681b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e13fc8f848d16327a1",
+    "url": "/static/js/9.ef67b4da.chunk.js"
+  },
+  {
+    "revision": "318159f60f161b29a7f9",
+    "url": "/static/js/main.b2a3a9f7.chunk.js"
+  },
+  {
+    "revision": "ac949a31cce3bf032cec",
+    "url": "/static/js/runtime-main.18f8f0e8.js"
+  },
+  {
+    "revision": "ec78e8e86a08b2278fa9baf09388d697",
+    "url": "/static/media/chef.ec78e8e8.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
