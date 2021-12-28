ad01a9dba6007ab99eb1e2a43180aa4284d46ef1
DiFi Era <83540733+jdefi@users.noreply.github.com>
2021-12-28 11:44:23 +0200

    Update Loader.tsx
    chang icon

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -11,7 +11,7 @@ const Loader: React.FC<LoaderProps> = ({ text }) => {
   return (
     <StyledLoader>
       <CardIcon>
-        <StyledSwap>🍣</StyledSwap>
+        <StyledSwap>🐔</StyledSwap>
       </CardIcon>
       {!!text && <StyledText>{text}</StyledText>}
     </StyledLoader>
